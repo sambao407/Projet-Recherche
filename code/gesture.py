ef0a344337291e04005b5ab8301cec0072116398
Nex45 <patricia.hu@viacesi.Fr>
2020-05-22 17:15:16 +0200

    Ajout du frame (carré vert)

--- code/gesture.py
+++ code/gesture.py
@@ -3,12 +3,25 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+# region of interest (ROI) coordinates
+(top, right, bottom, left) = 10, 350, 225, 590
+
+while (True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    (ret, frame) = cap.read()
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    frame = cv2.flip(frame, 1)
+
+    # get the ROI
+    roi = frame[top:bottom, right:left]
+
+    # draw the segmented hand
+    cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+    # display the frame with segmented hand
+    cv2.imshow("Camera Live", frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
