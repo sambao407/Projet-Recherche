cdbc131a44fd717afc399ca4331b8990cd3bd4ca
Nex45 <patricia.hu@viacesi.Fr>
2020-05-23 16:15:18 +0200

    Modification du main - capture-image.py Lien du GUI tools

--- code/capture-image.py
+++ code/capture-image.py
@@ -2,7 +2,7 @@ import cv2
 
 cam = cv2.VideoCapture(0)
 
-cv2.namedWindow("test")
+cv2.namedWindow("Camera")
 
 img_counter = 0
 
@@ -11,7 +11,7 @@ while True:
     if not ret:
         print("failed to grab frame")
         break
-    cv2.imshow("test", frame)
+    cv2.imshow("Camera", frame)
 
     k = cv2.waitKey(1)
     if k%256 == 27:
@@ -20,9 +20,9 @@ while True:
         break
     elif k%256 == 32:
         # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
+        img_name = "../data/p/hand_gesture_{}.png".format(img_counter)
         cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
+        print("{} created!".format(img_name))
         img_counter += 1
 
 cam.release()
