12c750bb8914872b64cef2719e9db82f31ed328d
sambao407 <sambao407@gmail.com>
2020-05-27 16:20:56 +0200

    Add Orlando Image

--- code/capture-image.py
+++ code/capture-image.py
@@ -4,7 +4,7 @@ cam = cv2.VideoCapture(0)
 
 cv2.namedWindow("Camera")
 
-img_counter = 0
+img_counter = 35
 
 while True:
     ret, frame = cam.read()
@@ -21,7 +21,7 @@ while True:
         break
     elif k%256 == 32:
         # SPACE pressed
-        img_name = "../data/n/hand_gesture_{}.png".format(img_counter)
+        img_name = "../data/p/hand_gesture_{}.png".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} created!".format(img_name))
         img_counter += 1
