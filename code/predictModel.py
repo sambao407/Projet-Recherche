3f66a124b95fd484f368fa5317f63de7038cbe0d
AlexandreTavernier <alexandredidierrene.tavernier@viacesi.fr>
2020-05-29 18:21:36 +0200

    modification on predictModel.py

--- code/predictModel.py
+++ code/predictModel.py
@@ -1,10 +1,14 @@
 import tensorflow as tf
 import cv2
+import numpy as np
 
 class_names = ["fingerLeft", "fingerRight", "fist", "none", "palm", "thumb"]
 
+to_predict = []
 num_frames = 0
 cam = cv2.VideoCapture(0)
+
+model = tf.keras.models.load_model("../data/trainModel/CNN.model")
 
 while True:
     ret, frame = cam.read()
@@ -13,20 +17,20 @@ while True:
     img_gray = cv2.cvtColor(frame, cv2.IMREAD_GRAYSCALE)
     img_resize = cv2.resize(img_gray, (img_size, img_size))
     img_resize = img_resize/255.0
-    img_reshape = img_resize.reshape(-1, img_size, img_size, 1)
+    to_predict.append(img_resize)
 
-    print(img_reshape)
 
-    model = tf.keras.models.load_model("../data/trainModel/CNN.model")
+    if len(to_predict) == 30:
+        frame_to_predict = np.array(to_predict, dtype=np.uint8)
+        frame_to_predict = frame_to_predict.reshape(-1, img_size, img_size, 1)
+        prediction = model.predict([frame_to_predict])
+        prediction = list(prediction[0])
+        class_name = class_names[prediction.index(max(prediction))]
 
-    prediction = model.predict([img_reshape])
-    prediction = list(prediction[0])
-    class_name = class_names[prediction.index(max(prediction))]
+        print(class_name)
 
-    print(class_name)
-
-    cv2.putText(frame, class_name, (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_4)
-    cv2.imshow('Hand Gesture Recognition', frame)
+        cv2.putText(frame, class_name, (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.imshow('Hand Gesture Recognition', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
