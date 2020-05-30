0e0088bbbd1d3faed10c56db8b47c63a214a280c
Nex45 <patricia.hu@viacesi.Fr>
2020-05-30 16:40:49 +0200

    reset de predictModel.py, résolvant la baisse de frame changement de bouton pour fermer la caméra

--- code/predictModel.py
+++ code/predictModel.py
@@ -1,10 +1,8 @@
 import tensorflow as tf
 import cv2
-import numpy as np
 
 class_names = ["fingerLeft", "fingerRight", "fist", "none", "palm", "thumb"]
 
-to_predict = []
 num_frames = 0
 cam = cv2.VideoCapture(0)
 
@@ -17,26 +15,25 @@ while True:
     img_gray = cv2.cvtColor(frame, cv2.IMREAD_GRAYSCALE)
     img_resize = cv2.resize(img_gray, (img_size, img_size))
     img_resize = img_resize/255.0
-    to_predict.append(img_resize)
+    img_reshape = img_resize.reshape(-1, img_size, img_size, 1)
 
+    print(img_reshape)
 
-    if len(to_predict) == 30:
-        frame_to_predict = np.array(to_predict, dtype=np.uint8)
-        frame_to_predict = frame_to_predict.reshape(-1, img_size, img_size, 1)
-        prediction = model.predict([frame_to_predict])
-        prediction = list(prediction[0])
-        class_name = class_names[prediction.index(max(prediction))]
+    prediction = model.predict([img_reshape])
+    prediction = list(prediction[0])
+    class_name = class_names[prediction.index(max(prediction))]
 
-        print(class_name)
+    print(class_name)
 
-        frame = cv2.flip(frame)
-        cv2.putText(frame, class_name, (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.imshow('Hand Gesture Recognition', frame)
+    frame = cv2.flip(frame,1)
+    cv2.putText(frame, class_name, (30, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_4)
+    cv2.imshow('Hand Gesture Recognition', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    k = cv2.waitKey(1)
+    if k % 256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
         break
 
 cam.release()
-cv2.destroyWindow()
-
-
+cv2.destroyAllWindows()
