0884baa27c3f1c5883d8287a1e1548795ad9935e
sambao407 <sambao407@gmail.com>
2020-05-30 16:14:37 +0200

    Add the file which run

--- code/predictModel.py
+++ code/predictModel.py
@@ -1,14 +1,10 @@
 import tensorflow as tf
 import cv2
-import numpy as np
 
 class_names = ["fingerLeft", "fingerRight", "fist", "none", "palm", "thumb"]
 
-to_predict = []
 num_frames = 0
 cam = cv2.VideoCapture(0)
-
-model = tf.keras.models.load_model("../data/trainModel/CNN.model")
 
 while True:
     ret, frame = cam.read()
@@ -17,21 +13,20 @@ while True:
     img_gray = cv2.cvtColor(frame, cv2.IMREAD_GRAYSCALE)
     img_resize = cv2.resize(img_gray, (img_size, img_size))
     img_resize = img_resize/255.0
-    to_predict.append(img_resize)
+    img_reshape = img_resize.reshape(-1, img_size, img_size, 1)
 
+    print(img_reshape)
 
-    if len(to_predict) == 30:
-        frame_to_predict = np.array(to_predict, dtype=np.uint8)
-        frame_to_predict = frame_to_predict.reshape(-1, img_size, img_size, 1)
-        prediction = model.predict([frame_to_predict])
-        prediction = list(prediction[0])
-        class_name = class_names[prediction.index(max(prediction))]
+    model = tf.keras.models.load_model("../data/trainModel/CNN.model")
 
-        print(class_name)
+    prediction = model.predict([img_reshape])
+    prediction = list(prediction[0])
+    class_name = class_names[prediction.index(max(prediction))]
 
-        frame = cv2.flip(frame)
-        cv2.putText(frame, class_name, (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.imshow('Hand Gesture Recognition', frame)
+    print(class_name)
+
+    cv2.putText(frame, class_name, (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_4)
+    cv2.imshow('Hand Gesture Recognition', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
