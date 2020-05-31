9a6c08141c0a970173e17b5985eeefe91e01898d
Nex45 <patricia.hu@viacesi.Fr>
2020-05-31 16:10:57 +0200

    mep ROI - predictModel.py ajout de mes DataSets fond neutre

--- code/predictModel.py
+++ code/predictModel.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import cv2
+# from PIL import crop
 
 class_names = ["fingerLeft", "fingerRight", "fist", "none", "palm", "thumb"]
 
@@ -8,6 +9,11 @@ cam = cv2.VideoCapture(0)
 
 #Load the neural network model
 model = tf.keras.models.load_model("../data/trainModel/CNN.model")
+
+#Region of interest (ROI) coordinates
+top, right, bottom, left = 60, 420, 225, 590
+
+start_recording = False
 
 while True:
     #Getting frame by frame
@@ -24,14 +30,21 @@ while True:
     prediction = model.predict([img_reshape])
     prediction = list(prediction[0])
     class_name = class_names[prediction.index(max(prediction))]
-    print(class_name)
+    print(prediction)
 
     #Flip the frame
     frame = cv2.flip(frame,1)
+
+    #Get the ROI
+    roi = frame[top:bottom, right:left]
+    #Draw the ROI
+    cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
     #Write the classname on the frame and show it
     cv2.putText(frame, class_name, (30, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_4)
     cv2.imshow('Hand Gesture Recognition', frame)
+    cv2.imshow('Class Detection', roi)
 
+    #Define frame exit key
     k = cv2.waitKey(1)
     if k % 256 == 27:
         # ESC pressed
